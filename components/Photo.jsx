7c4fea464199844905016b639c6c94f8c796c242
francovinicius <duartevini.dev@gmail.com>
2024-09-17 08:42:18 -0300

    Create: - border image

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -15,10 +15,26 @@ const Photo = () => {
                     initial={{ opacity: 0 }}
                     animate={
                         { opacity: 1, transition: { delay: 1.4, duration: 0.4, ease: "easeInOut" } }
-                    }                
-                className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-exclusion'>
+                    }
+                    className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-exclusion absolute'>
                     <img src='/assets/Foto.svg' priority quality={100} fill alt='' className='object-contain' />
                 </motion.div>
+
+                <motion.svg className='w-[300px] xl:w-[500px] h-[300px] xl:h-[500px]' fill='transparent' viewBox='0 0 506 506'
+                    xmlns='http://www.w3.org/2000/svg'>
+                    <motion.circle cx='253' cy='253' r='250' stroke='#00ff99' strokeLinecap='round' strokeLinejoin='round'
+                    initial={{strokeDasharray: "24 10 0 0"}}
+                    animate={{
+                        strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
+                        rotate: [120,360],
+                    }}
+                    transition={{
+                        duration: 20,
+                        repeat: Infinity,
+                        repeatType: 'reverse'
+
+                    }}/>
+                </motion.svg>
             </motion.div>
         </div>
     )
