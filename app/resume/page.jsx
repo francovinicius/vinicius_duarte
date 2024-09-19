b19a1939532eda389ed16905f4cc9a813cbd78fb
francovinicius <duartevini.dev@gmail.com>
2024-09-19 20:49:06 -0300

    Update: - Resume Page -- Remove: Skills and About Buttons -- Alt Experience and Education Buttons

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -4,61 +4,33 @@ import { FaHtml, FaCss3, FaJS, FaReact, FaFimga, FaNodeJs } from 'react-icons/fa
 import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
 import React from 'react'
 
-const about = {
-  title: 'About me',
-  description: 'Loren Ipson',
-  info: [
-    {
-      fieldName: 'Name',
-      fieldValue: 'Vinicius Duarte',
-    },
-    {
-      fieldName: 'Phone',
-      fieldValue: '24 99987-9994',
-    },
-    {
-      fieldName: 'Experience',
-      fieldValue: '+2 Anos',
-    },
-    {
-      fieldName: 'Email',
-      fieldValue: 'duartevini.dev@gmail.com',
-    },
-    {
-      fieldName: 'Languages',
-      fieldValue: 'Português, Inglês',
-    },
-  ]
-}
-
 const experience = {
   icon: '/assets/resume/badge.svg',
   title: 'Experiências',
-  description: 'Minhas Experiências Profissionais',
+  description: 'Pós Graduando em Full Stack Developer, atuando a um pouco mais de 2 anos de como desenvolvedor Web/Mobile, entreguei cerca de 17 projetos (tanto Web quanto Mobile) entregues como freelancer. ',
+  descriptionTwo: 'Meus Skills: HTML5, CSS3, Git/Github, Figma, JavaScript, ReactJs, NodeJs, Dart, Flutter, Bootstrap, Tailwind, Kotlin, PHP, WordPress, Integração com API, SQL, MySQL, Firebase, Excel, Google Sheets e Forms.',
+  descriptionThree: 'Abaixo apresento-lhes alguns lugares um fiz/faço parte da equipe juntamente com minha função:',
   items: [
     {
       company: "Ajr Puxadas",
       position: 'Analista de sistemas',
-      duaration: 'Mai/2022 à Presente'
+      duaration: 'Mai/2022 o Momento',
 
     },
     {
       company: "Freelancer",
       position: 'Desenvolvedor Web/Mobile',
-      duaration: 'Mai/2022 à Presente'
-
+      duaration: 'Mai/2022 o Momento',
     },
     {
       company: "Youtility",
       position: 'Supervisor de operações',
-      duaration: 'Mar/2019 à Mar/2020'
-
+      duaration: 'Mar/2019 à Mar/2020',
     },
     {
       company: "UGB - Centro Universitário Geraldo di Biase",
       position: 'Estágio de Redes e Manutenção',
-      duaration: 'Jul/2014 - Jan/2017'
-
+      duaration: 'Jul/2014 - Jan/2017',
     },
   ]
 }
@@ -66,12 +38,12 @@ const experience = {
 const education = {
   icon: 'assets/resume/cap.svg',
   title: 'Formação',
-  description: 'Minhas Experiências Profissionais',
+  description: 'Hoje estou cursando Pós-Graduação em Desenvolvimento Full Stack na PUC Minas, porém em minha tragetória me graduei em Matemática e Análise e Desenvolvimento de Sistemas nas universidades abaixo mencionadas:',
   items: [
     {
-      instituition: "Centro Universitário Geraldo di Biase",
-      degree: 'Graduado em Matemática',
-      duaration: 'Jan/13 - Dez/16'
+      instituition: "Pontifícia Universidade Católica",
+      degree: 'Pós-Graduando em Full Stack',
+      duaration: 'Set/24 - Set/25'
 
     },
     {
@@ -81,9 +53,9 @@ const education = {
 
     },
     {
-      instituition: "Pontifícia Universidade Católica",
-      degree: 'Pós-Graduando em Full Stack',
-      duaration: 'Set/24 - Set/25'
+      instituition: "Centro Universitário Geraldo di Biase",
+      degree: 'Graduado em Matemática',
+      duaration: 'Jan/13 - Dez/16'
 
     },
   ]
@@ -125,14 +97,8 @@ const Resume = () => {
             <TabsTrigger value='experience'>
               Experience
             </TabsTrigger>
-            <TabsTrigger value='skills'>
-              Skills
-            </TabsTrigger>
             <TabsTrigger value='education'>
               Education
-            </TabsTrigger>
-            <TabsTrigger value='about'>
-              About
             </TabsTrigger>
           </TabsList>
 
@@ -141,30 +107,41 @@ const Resume = () => {
               <div className='flex flex-col gap-[30px] text-center xl:text-left'>
                 <h3>{experience.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                  {experience.description}
+                  {experience.description} <br /> {experience.descriptionTwo} <br /><br /> {experience.descriptionThree}
                 </p>
                 <ScrollArea className='h-[400px]'>
                   <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
                     {experience.items.map((item, index) => {
-                      return <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-center lg:items-start gap-1'>
-                        <h3>{item.position}</h3>
-                        <span></span>
+                      return <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
+                        <h3 className='mb-3'>{item.position}</h3>
+                        <span>{item.skills}</span>
                         <p>{item.company}</p>
+                        <span>{item.duaration}</span>
+                      </li>
+                    })}
+                  </ul>
+                  <br />
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value='education' className='w-full'>
+              <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                <h3>{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea className='h-[400px]'>
+                  <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
+                    {education.items.map((item, index) => {
+                      return <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
+                        <h3 className='mb-3'>{item.degree}</h3>
+                        <span>{item.instituition}</span>
                         <span>{item.duaration}</span>
                       </li>
                     })}
                   </ul>
                 </ScrollArea>
               </div>
-            </TabsContent>
-            <TabsContent value='skills' className='w-full'>
-              y
-            </TabsContent>
-            <TabsContent value='education' className='w-full'>
-              aboutt
-            </TabsContent>
-            <TabsContent value='about' className='w-full'>
-              about
             </TabsContent>
           </div>
         </Tabs>
